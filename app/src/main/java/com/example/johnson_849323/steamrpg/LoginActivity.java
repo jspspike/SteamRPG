07c9c11c81ca009ffcff3b6c4bcd9fdee64e55fb
Joshua Johnson <jspspike@gmail.com>
2016-04-07 14:12:03 -0500

    Wrote webActivity to get steam id of user

---
+++ app/src/main/java/com/example/johnson_849323/steamrpg/LoginActivity.java
@@ -0,0 +1,83 @@
+package com.example.johnson_849323.steamrpg;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+
+public class LoginActivity extends Activity {
+
+
+    public static final String WEBSITE = "SteamRPG";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+        final WebView webView = new WebView(this);
+        webView.getSettings().setJavaScriptEnabled(true);
+
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                setTitle(url);
+                Uri Url = Uri.parse(url);
+
+                if(Url.getAuthority().equals(WEBSITE.toLowerCase())) {
+                    webView.stopLoading();
+
+                    Uri userAccountUrl = Uri.parse(Url.getQueryParameter("openid.identity"));
+                    String userID = userAccountUrl.getLastPathSegment();
+
+                    TextView textView = (TextView) findViewById(R.id.textView);
+                    textView.setText(userID);
+                }
+            }
+        });
+
+       setContentView(webView);
+
+        String url = "https://steamcommunity.com/openid/login?openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select" +
+                "&openid.identity=http://specs.openid.net/auth/2.0/identifier_select" +
+                "&openid.mode=checkid_setup" +
+                "&openid.ns=http://specs.openid.net/auth/2.0" +
+                "&openid.realm=https://" + WEBSITE +
+                "&openid.return_to=https://" + WEBSITE + "/signin/";
+
+        webView.loadUrl(url);
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
--- app/src/main/java/com/example/johnson_849323/steamrpg/MainActivity.java
+++ app/src/main/java/com/example/johnson_849323/steamrpg/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.johnson_849323.steamrpg;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +13,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainActivity.this.startActivity(new Intent(MainActivity.this, LoginActivity.class));
     }
 
 
