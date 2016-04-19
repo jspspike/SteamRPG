5b582ffae0221745526821f9343b3e4f508a9610
Joshua Johnson <jspspike@jspspsike>
2016-04-19 13:25:09 -0500

    Fixed compiling issues

--- app/src/main/java/com/example/johnson_849323/steamrpg/LoginActivity.java
+++ app/src/main/java/com/example/johnson_849323/steamrpg/LoginActivity.java
@@ -86,10 +86,6 @@ public class LoginActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         return super.onOptionsItemSelected(item);
     }
 }
