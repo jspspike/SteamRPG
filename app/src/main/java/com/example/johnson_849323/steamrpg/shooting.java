088fa8f6c9cdc2f8acd33b7dff4b1f27cd3952dc
Formicidae <eddie9ner@gmail.com>
2016-04-20 10:34:18 -0500

    Added Shared preferences
    Only fro shooting and in main activity, will need to find a way to pass
    values back.

--- app/src/main/java/com/example/johnson_849323/steamrpg/shooting.java
+++ app/src/main/java/com/example/johnson_849323/steamrpg/shooting.java
@@ -5,6 +5,7 @@ import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
